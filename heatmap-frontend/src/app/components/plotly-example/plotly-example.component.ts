ce561f18c06622126b6ddf67e9682524f007b61c
Ilia Nikolaenko <niil02@sms-group.com>
2022-11-17 10:50:09 +0100

    Upd the test plotly component

--- heatmap-frontend/src/app/components/plotly-example/plotly-example.component.ts
+++ heatmap-frontend/src/app/components/plotly-example/plotly-example.component.ts
@@ -17,7 +17,7 @@ export class PlotlyExampleComponent implements OnInit {
         { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: {color: 'red'} },
         { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
     ],
-    layout: {width: 320, height: 240, title: 'A Fancy Plot'}
+    layout: {autosize: true, title: 'A Fancy Plot'}
 };
 
 }
