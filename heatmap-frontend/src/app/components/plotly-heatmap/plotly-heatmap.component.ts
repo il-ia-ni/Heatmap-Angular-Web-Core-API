d2670536e344ad32132e747b9afd470c8629725e
Ilia Nikolaenko <niil02@sms-group.com>
2022-11-17 12:14:45 +0100

    FE: Added test logic for generating a heatmap

--- heatmap-frontend/src/app/components/plotly-heatmap/plotly-heatmap.component.ts
+++ heatmap-frontend/src/app/components/plotly-heatmap/plotly-heatmap.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-plotly-heatmap',
   templateUrl: './plotly-heatmap.component.html',
   styleUrls: ['./plotly-heatmap.component.scss']
 })
-export class PlotlyHeatmapComponent {
+export class PlotlyHeatmapComponent implements OnInit {
 
+  private xTicks: string[] = [];
+  private yTicks: string[] = [];
+  private zValues: Array<number>[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.generateData();
+  }
+
+  public heatmap = {
+    data: [
+      {
+        x: this.xTicks,
+        y: this.yTicks,
+        z: this.zValues,
+        type: 'heatmap'
+      }
+    ],
+    layout: {autosize: true, title: 'A generated heatmap'}
+  }
+
+  private generateData() {
+    let i = 30;
+    let length = 50;
+    while (i >= 0) {
+      let temp: number[] = [];
+      for (let j = 0; j <= length; j++) {
+        temp.push(Math.ceil(Math.random() * 100));
+      }
+      this.zValues.push(temp);
+      i--;
+    }
+
+    while (i < this.zValues.length) {
+      this.yTicks[i] = "y" + i;
+      i++;
+    }
+
+    i = 0;
+    while (i < this.zValues[0].length) {
+      this.xTicks[i] = "x" + i;
+      i++;
+    }
+
+  }
 
 }
