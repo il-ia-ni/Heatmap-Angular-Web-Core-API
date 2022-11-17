5122d8c84b81ab92cbe04e0f0a5473f1023bab11
Ilia Nikolaenko <niil02@sms-group.com>
2022-11-17 10:25:46 +0100

    FE: Set up project to use Plotly lib

--- heatmap-frontend/src/app/app.module.ts
+++ heatmap-frontend/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import * as PlotlyJS from 'plotly.js-dist-min';
+import { PlotlyModule } from 'angular-plotly.js';
+
+PlotlyModule.plotlyjs = PlotlyJS;
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PlotlyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
